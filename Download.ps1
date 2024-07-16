056e25e2b9f09ce01f810b3f14f50c6a2a12ec45
NoahGori <noahgori@gmail.com>
2024-07-15 22:03:20 -0600

    change output

--- Download.ps1
+++ Download.ps1
@@ -28,7 +28,7 @@ New-Item $DotfilesFolder -ItemType directory;
 
 # Download Dotfiles repository as Zip
 Try {
-  Invoke-WebRequest $GitHubRepositoryUri -O $ZipRepositoryFile;
+  Invoke-WebRequest $GitHubRepositoryUri -OutFile $ZipRepositoryFile;
   $DownloadResult = $TRUE;
 }
 catch [System.Net.WebException] {
