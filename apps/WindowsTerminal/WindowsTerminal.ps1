bd284b613a4739f7695f79b4986bbc4ff2749913
NoahGori <noahrgori@gmail.com>
2024-07-18 01:09:07 -0600

    remove open new terminal

--- apps/WindowsTerminal/WindowsTerminal.ps1
+++ apps/WindowsTerminal/WindowsTerminal.ps1
@@ -43,13 +43,6 @@ function Set-WindowsTerminal-Settings {
   Write-Host "Windows Terminal was successfully configured." -ForegroundColor "Green";
 }
 
-function Open-WindowsTerminal {
-  # Open and close Windows Terminal as admin to load the profile
-  Write-Host "Opening Windows Terminal for 10 seconds:" -ForegroundColor "Green";
-  wt new-tab PowerShell -c "Set-ExecutionPolicy Unrestricted;";
-
-  Start-Sleep -Seconds 10;
-}
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
@@ -60,4 +53,3 @@ Install-Module -Name "PSReadLine" -Repository "PSGallery" -RequiredVersion 2.1.0
 Set-OhMyPosh-Theme;
 Set-PowerShell-Profile;
 Set-WindowsTerminal-Settings;
-Open-WindowsTerminal;
